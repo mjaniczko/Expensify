3dd2ed79ebb1609be825ad4fbdd89015898f2be9
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-01 21:43:22 +0200

    Template to testing router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is form my dashboard component
+  </div>
+)
+
+const AddExpensePage = () => (
+  <div>
+    This is form my add expense component
+  </div>
+)
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact/>
+      <Route path="/create" component={AddExpensePage}/>
+    </div>
+  </BrowserRouter>
+)
+
+
+ReactDOM.render(routes, document.getElementById("app"));
