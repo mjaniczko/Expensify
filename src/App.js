3f22f8f91fa52bf48b5d36cfe596430c5ea8a204
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-02 18:08:27 +0200

    Discovering navlink and link react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
@@ -19,17 +19,28 @@ const AddExpensePage = () => (
 
 const NotFoundPage = () => (
   <div>
-    404! Page not found
+    404! Page not found - <Link to='/'>GO HOME</Link>
   </div>
+)
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to='/' activeClassName="is-active" exact>HOME</NavLink>
+    <NavLink to='/create' activeClassName="is-active">CREATE EXPENSE</NavLink>
+  </header>
 )
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact/>
-      <Route path="/create" component={AddExpensePage}/>
-      <Route component={NotFoundPage}/>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact/>
+        <Route path="/create" component={AddExpensePage}/>
+        <Route component={NotFoundPage}/>
+      </Switch>
+    </div>
   </BrowserRouter>
 )
 
