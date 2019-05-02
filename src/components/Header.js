dcfb8f4a3c2bb93e4e5f19384cdbb342ceb9dbcc
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-02 18:47:16 +0200

    Remove EditExpensePage component NavLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
     <h1>Expensify</h1>
     <NavLink to='/' activeClassName="is-active" exact>HOME</NavLink>
     <NavLink to='/create' activeClassName="is-active">CREATE EXPENSE</NavLink>
-    <NavLink to='/edit' activeClassName="is-active">EDIT EXPENSE</NavLink>
     <NavLink to='/help' activeClassName="is-active">HELP</NavLink>
   </header>
 )
