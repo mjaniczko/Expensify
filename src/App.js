d72b6577749490c4a44bf2cb30de8f38ce89a291
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-02 18:29:11 +0200

    Import AppRouter component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 
+import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is form my dashboard component
-  </div>
-)
 
-const AddExpensePage = () => (
-  <div>
-    This is form my add expense component
-  </div>
-)
-
-const NotFoundPage = () => (
-  <div>
-    404! Page not found - <Link to='/'>GO HOME</Link>
-  </div>
-)
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to='/' activeClassName="is-active" exact>HOME</NavLink>
-    <NavLink to='/create' activeClassName="is-active">CREATE EXPENSE</NavLink>
-  </header>
-)
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact/>
-        <Route path="/create" component={AddExpensePage}/>
-        <Route component={NotFoundPage}/>
-      </Switch>
-    </div>
-  </BrowserRouter>
-)
-
-
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
