48f50425590ef85cfa185aface75faa447a0fba4
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-04 19:30:47 +0200

    Testing data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+import { Provider } from 'react-redux'
 
 import configureStore from './store/configureStore'
 import AppRouter from './routers/AppRouter'
+import './styles/styles.scss'
 
 import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 
-import 'normalize.css/normalize.css'
-import './styles/styles.scss'
 
 const store = configureStore();
 
@@ -17,8 +18,20 @@ store.dispatch(addExpense({ description: 'Water bill '}))
 store.dispatch(addExpense({ description: 'Gas bill '}))
 store.dispatch(setTextFilter('water'))
 
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'))
+}, 3000)
+
+
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById("app"));
