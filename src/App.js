92bffdd9b1a3dd035e46191943942a8d3de02b87
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-04 07:56:17 +0200

    Testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 
+import configureStore from './store/configureStore'
 import AppRouter from './routers/AppRouter'
+
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
+
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill '}))
+store.dispatch(addExpense({ description: 'Gas bill '}))
+store.dispatch(setTextFilter('water'))
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
