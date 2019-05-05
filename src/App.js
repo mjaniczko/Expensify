82978121bd47b4717b4560a4542d728d6fdd1465
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-05 06:50:29 +0200

    Removed part of the dummy data

--- src/App.js
+++ src/App.js
@@ -8,20 +8,14 @@ import AppRouter from './routers/AppRouter'
 import './styles/styles.scss'
 
 import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill '}))
-store.dispatch(addExpense({ description: 'Gas bill '}))
-store.dispatch(setTextFilter('water'))
-
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('rent'))
-}, 3000)
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
 
 
 const state = store.getState()
