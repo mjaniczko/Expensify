7f13812301241565830c0e798e36a0c999cb9639
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-05 09:33:11 +0200

    added constructor

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -5,13 +5,17 @@ import 'react-dates/lib/css/_datepicker.css'
 
 
 export default class ExpenseForm extends React.Component {
-  state = {
-    description: '',
-    note: '',
-    amount: '',
-    createdAt: moment(),
-    calendarFocused: false,
-    error: ''
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calendarFocused: false,
+      error: ''
+    }
   }
 
   onDescriptionChange = (e) => {
