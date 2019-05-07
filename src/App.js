991bf248fa0717e3848412be72dc4a63967432a2
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-07 10:55:22 +0200

    Moved css import

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
 import AppRouter from './routers/AppRouter'
 import './styles/styles.scss'
+import 'react-dates/lib/css/_datepicker.css'
 
 import { addExpense } from './actions/expenses'
-import getVisibleExpenses from './selectors/expenses'
 
 
 const store = configureStore();
@@ -17,10 +17,6 @@ store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
 store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}))
 store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
 
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
 
 const jsx = (
   <Provider store={store}>
