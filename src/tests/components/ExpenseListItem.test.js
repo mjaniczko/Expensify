8b40e093fd9e595c23f32593fa8d8a6d7268460a
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-07 10:06:41 +0200

    Testing ExpenseListItem component

---
+++ src/tests/components/ExpenseListItem.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import expenses from '../fixtures/expenses'
+import ExpenseListItem from '../../components/ExpenseListItem'
+
+test('Should render ExpenseListItem', () => {
+  const wrapper = shallow(<ExpenseListItem {...expenses[1]} />)
+  expect(wrapper).toMatchSnapshot()
+})
+
