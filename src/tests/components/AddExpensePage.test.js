6557f79c3ac278da6347ee45bc8e6304e741f3cb
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-08 09:20:22 +0200

    Change some of onSubmit prop into addExpense

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -5,12 +5,12 @@ import { AddExpensePage } from '../../components/AddExpensePage'
 import expenses from '../fixtures/expenses'
 
 
-let onSubmit, history, wrapper
+let addExpense, history, wrapper
 
 beforeEach(() => {
-  onSubmit = jest.fn()
+  addExpense = jest.fn()
   history = { push: jest.fn() }
-  wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>)
+  wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history}/>)
 })
 
 test('Should render AddExpensePage correctly', () => {
@@ -20,5 +20,5 @@ test('Should render AddExpensePage correctly', () => {
 test('Should handle onSubmit', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
   expect(history.push).toHaveBeenLastCalledWith('/')
-  expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
+  expect(addExpense).toHaveBeenLastCalledWith(expenses[1])
 })
