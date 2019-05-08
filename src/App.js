d64a5283331269d1e6773185dcfc70725916a3ba
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-08 10:55:30 +0200

    Removed dummy data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'normalize.css/normalize.css'
 import { Provider } from 'react-redux'
+import 'normalize.css/normalize.css'
 
 import configureStore from './store/configureStore'
 import AppRouter from './routers/AppRouter'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 
-import { addExpense } from './actions/expenses'
-
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}))
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
 
 
 const jsx = (
