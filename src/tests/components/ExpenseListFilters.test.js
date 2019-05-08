68c127381c536b11bceed4772763403265459e65
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-08 10:42:10 +0200

    Ending testing ExpenseListFilters

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import moment from 'moment'
 
 import { ExpenseListFilters } from '../../components/ExpenseListFilters'
 import { filters, altFilters } from '../fixtures/filters'
@@ -25,7 +26,6 @@ beforeEach(() => {
   )
 })
 
-
 test('Should render ExpenseListFilters correctly', () => {
   expect(wrapper).toMatchSnapshot()
 })
@@ -37,6 +37,43 @@ test('Should render ExpenseListFilters with alt data correctly', () => {
   expect(wrapper).toMatchSnapshot()
 })
 
+test('Should handle text change', () => {
+  const value = 'rent'
+  wrapper.find('input').simulate('change', {
+    target: { value }
+  })
+  expect(setTextFilter).toHaveBeenLastCalledWith(value)
+})
 
+test('Should sort by date', () => {
+  wrapper.setProps({
+    filters: altFilters
+  })
+  const value = 'date'
+  wrapper.find('select').simulate('change', {
+    target: { value }
+  })
+  expect(sortByDate).toHaveBeenCalled()
+})
 
-test('', () => {})
+test('Should sort by amount', () => {
+  const value = 'amount'
+  wrapper.find('select').simulate('change', {
+    target: { value }
+  })
+  expect(sortByAmount).toHaveBeenCalled()
+})
+
+test('Should handle date changes', () => {
+  const startDate = moment(0).add(30, 'years')
+  const endDate = moment(0).add(47, 'years')
+  wrapper.find('DateRangePicker').prop('onDatesChange')({ startDate, endDate })
+  expect(setStartDate).toHaveBeenLastCalledWith(startDate)
+  expect(setEndDate).toHaveBeenLastCalledWith(endDate)
+})
+
+test('Should handle date focus changes', () => {
+  const calendarFocused = 'endDate'
+  wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused)
+  expect(wrapper.state('calendarFocused')).toBe(calendarFocused)
+})
