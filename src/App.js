e7f2a0e99c2964eefbe13e93deb5ecd148dc5208
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-09 14:42:24 +0200

    Conditional dom rendering

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import configureStore from './store/configureStore'
 import AppRouter from './routers/AppRouter'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
+import { startSetExpenses } from './actions/expenses'
 
 import './firebase/firebase'
 
@@ -18,5 +19,8 @@ const jsx = (
     <AppRouter />
   </Provider>
 )
+ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-ReactDOM.render(jsx, document.getElementById("app"));
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById("app"));
+})
