b4c23329359b1a05983283a433e2a5861ff6b916
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-09 15:49:49 +0200

    Using edit expense connected into firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,7 +33,7 @@ export const removeExpense = ({ id } = {}) => ({
 })
 
 export const startRemoveExpense = ({ id } = {}) => {
-  return(dispatch) => {
+  return (dispatch) => {
     return database.ref(`expenses/${id}`).remove().then(() => {
       dispatch(removeExpense({ id }))
     })
@@ -47,6 +47,14 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 })
+
+export const startEditExpense = (id, updates) => {
+  return(dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+      dispatch(editExpense(id, updates))
+    })
+  }
+}
 
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
