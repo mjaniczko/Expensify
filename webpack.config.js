0cceeba7d022b1c6273a70d50b36c0ad2913113e
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-10 15:05:25 +0200

    Babel polyfill added into config

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (env) => {
   const CSSExtract = new ExtractTextPlugin('styles.css')
 
   return {
-    entry: './src/App.js',
+    entry: ['babel-polyfill', './src/App.js'],
     output: {
     path: path.join(__dirname, 'public', 'dist'),
     filename: 'bundle.js'
