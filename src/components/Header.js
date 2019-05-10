b223ebb49a0c25b66dff153ea67c07e4ea9d5dce
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-10 09:34:45 +0200

    added logout button removed help page component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Header = () => (
+import { startLogout } from '../actions/auth'
+
+
+export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to='/' activeClassName="is-active" exact>HOME</NavLink>
+    <NavLink to='/dashboard' activeClassName="is-active">DASHBOARD</NavLink>
     <NavLink to='/create' activeClassName="is-active">CREATE EXPENSE</NavLink>
-    <NavLink to='/help' activeClassName="is-active">HELP</NavLink>
+    <button onClick={startLogout}>Logout</button>
   </header>
 )
 
-export default Header
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout())
+})
+
+
+export default connect(undefined, mapDispatchToProps)(Header)
